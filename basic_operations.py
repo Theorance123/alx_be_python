6b6005dac3332a41cb25ce728224ab173e588fb1
Theorance123 <132573317+Theorance123@users.noreply.github.com>
2024-09-22 14:09:15 +0200

    basic_operations.py

---
+++ basic_operations.py
@@ -0,0 +1,11 @@
+# Define variables
+number1 = 10 
+nummber2 = 5 
+
+addition = number1 + nummber2
+subtraction = number1 - nummber2
+multiplication = number1 * nummber2
+
+print(f"{addition} addition of number1 and number2")
+print(f"{subtraction} subtraction of number1 and nummber2")
+print(f"{multiplication} multipliccation of number1 and number2")
